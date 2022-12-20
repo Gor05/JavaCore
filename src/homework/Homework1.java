72de17bb40e87b008a7e2100dace72114872d5a9
Gor05 <gor.melikyan1@tumo.org>
2022-12-20 19:22:40 +0300

    add some examples

---
+++ src/homework/Homework1.java
@@ -0,0 +1,34 @@
+package homework;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+        //ֆիգուռ 1
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//----------------------------------------------------------------------------------------------------------------------
+        // ֆիգուռ 2
+//        for (int i = 5; i > 0; i--) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+        //--------------------------------------------------------------------------------------------------------------
+        // ֆիգուռ 3
+        int i,j,k;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print("");
+            }
+            for (k = 1;k <= i;k++){
+                System.out.println("*");
+            }
+        }
+    }
+}
