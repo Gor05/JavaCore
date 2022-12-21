f4a1153ce307b3b9c3ca804fd252b4e7658ff430
Gor05 <gor.melikyan1@tumo.org>
2022-12-21 17:54:53 +0300

    add some examples

--- src/homework/Homework1.java
+++ src/homework/Homework1.java
@@ -4,21 +4,21 @@ public class Homework1 {
     public static void main(String[] args) {
 
         //ֆիգուռ 1
-//        for (int i = 0; i < 5; i++) {
-//            for (int j = 0; j <= i; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 //----------------------------------------------------------------------------------------------------------------------
         // ֆիգուռ 2
-//        for (int i = 5; i > 0; i--) {
-//            for (int j = 0; j < i; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//
-//        }
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
         //--------------------------------------------------------------------------------------------------------------
         // ֆիգուռ 3
         int i,j,k;
